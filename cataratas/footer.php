0cd11fc45469633bec3806444fe9380537567ad5
ArianSR29 <green-m-an@hotmail.com>
2017-11-03 12:09:16 -0600

    Implementación de los detalles finales

--- cataratas/footer.php
+++ cataratas/footer.php
@@ -1,5 +1,10 @@
     <section class="footer">
-        <span>© 2017 salauno®</span> <span class="divider-footer">|</span><img src="img/icon-telefono.svg" class="icon-size" alt="icon-telefono"> <span> 55 4163 7477</span> (Teléfono fijo)
+        <span>© 2017 salauno®</span> <span class="divider-footer">|</span><img src="img/icon-telefono.svg" class="icon-size" alt="icon-telefono"> <span> 55 4163 7477</span>
+        (Teléfono fijo)
+    </section>
+    <section class="footer-mobile">
+        <img src="img/icon-telefono.svg" class="icon-size" alt="icon-telefono"> <span> 55 4163 7477</span> (Teléfono fijo)
+        <br><span>© 2017 salauno®</span>
     </section>
 </body>
 </html>
