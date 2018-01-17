7e99e5c9a207c11671151a035129027e81202565
ArianSR29 <green-m-an@hotmail.com>
2018-01-17 15:04:00 -0600

    GlaucomaLP

---
+++ glaucoma/glaucoma.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Glaucoma</title>
+
+    <link rel="stylesheet" href="src/css/glaucoma.css">
+    <link rel="stylesheet" href="src/css/glaucoma.mobile.css">
+    
+</head>
+<body>
+    <div class="banner-section">
+        <div class="info-banner-section">
+            <div class="info-banner">
+                <div class="banner-logo"><img src="src/img/salauno-logo.svg" alt=""></div>
+                <div class="info">
+                    <div class="info-titulo"><span class="fs45 dinnB">Glaucoma</span></div>
+                    <div class="info-subtitulo"><span class="fs36 dinnB">Obtén un diagnóstico preciso y preserva tu visión</span></div>
+                    <div class="info-const"><span class="fs36 dinnB">Costo $350 MXN</span></div>
+                    <div>
+                        <a href="#" class="button-agenda-cita open-acuity-form" id="agenda">
+                            <div class="inline middle icon-agenda"><img src="src/img/icon-libro.svg" alt="icon-libro"></div>
+                            <div class="inline middle text-button dinnB fs24">Agenda tu cita</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="tipos-section">
+        <div class="tipos-titulo"><span class="fs32 dinnB">¿Cuáles son los tipos de glaucoma más comunes?</span></div>
+        <div class="tipos-item-section">
+            <div class="tipos-item"><img src="src/img/glaucoma-angulo-abierto.svg" alt="">
+                    <h3 class="fs24 dinnB">Glaucoma de ángulo abierto</h3>
+                    <span class="fs18 dinnR">El más común; hace que la presión del ojo incremente de forma gradual, dañando el nervio óptico. Su progreso es tan lento que podrías perder tu visión sin tener ningún síntoma adicional.</span>
+            </div>
+            <div class="tipos-item"><img src="src/img/glaucoma-angulo-cerrado_1.svg" alt="">
+                    <h3 class="fs24 dinnB">Glaucoma de ángulo cerrado</h3>
+                    <span class="fs18 dinnR">Es poco frecuente pero causa una alteración repentina de la visión, 
+                        acompañado de síntomas como dolor ocular, naúseas y halos alrededor de las luces.</span>
+            </div>
+            <div class="tipos-item"><img src="src/img/glaucoma-tension-normal.svg" alt="">
+                    <h3 class="fs24 dinnB">Glaucoma de tensión normal</h3>
+                    <span class="fs18 dinnR">Los ojos tienen una presión normal, sin embargo el nervio óptico se 
+                        daña produciendo una pérdida de la visión periférica. Su origen es desconocido.</span>
+            </div>
+            <div class="tipos-item"><img src="src/img/glaucoma-congenito.svg" alt="">
+                    <h3 class="fs24 dinnB">Glaucoma congénito</h3>
+                    <span class="fs18 dinnR">Es poco frecuente y aparece entre el nacimiento y los 3 años de edad por defectos del drenaje ocular, 
+                        con síntomas como ojos opacos, sensibilidad a la luz y lagrimeo excesivo.</span>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="enfermedades-sect">
+        <div class="title-enfermedades block">
+            <h1>¿Cuáles son los tipos de glaucoma más comunes?</h1>
+        </div>
+        <div class="enfermedades-item-sect block">
+            <div class="enfermedades-item"><img src="src/img/glaucoma-angulo-abierto.svg" alt="">
+                <h4>Glaucoma de ángulo abierto</h4>
+                <p>El más común; hace que la presión del ojo incremente de forma gradual, dañando el nervio óptico. 
+                    Su progreso es tan lento que podrías perder tu visión sin tener ningún síntoma adicional.</p>
+            </div>
+            <div class="enfermedades-item"><img src="src/img/glaucoma-angulo-cerrado_1.svg" alt="">
+                <h4>Glaucoma de ángulo cerrado</h4>
+                <p>Es poco frecuente pero causa una alteración repentina de la visión, 
+                acompañado de síntomas como dolor ocular, naúseas y halos alrededor de las luces.</p>
+            </div>
+            <div class="enfermedades-item"><img src="src/img/glaucoma-tension-normal.svg" alt="">
+                <h4>Glaucoma de tensión normal</h4>
+                <p>Los ojos tienen una presión normal, sin embargo el nervio óptico se 
+                daña produciendo una pérdida de la visión periférica. Su origen es desconocido.</p>
+            </div>
+            <div class="enfermedades-item"><img src="src/img/glaucoma-congenito.svg"  alt="">
+                <h4>Glaucoma congénito</h4>
+                <p>Es poco frecuente y aparece entre el nacimiento y los 3 años de edad por defectos del drenaje ocular, 
+                con síntomas como ojos opacos, sensibilidad a la luz y lagrimeo excesivo.</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+    <div class="confiar-section">
+        <div class="confiar-titulo"><span class="fs36 dinnB">¿Por qué confiar tus ojos a salauno?</span></div>
+        <div class="confiar-item-sect">
+            <div class="confiar-item">
+                <img src="src/img/medicos-certificados.svg" alt="">
+                <div class="confiar-info"><span class="fs24 dinnB">Oftalmólogos certificados</span></div>
+            </div>
+            <div class="confiar-item">
+                <img src="src/img/atencion-personalizada.svg" alt="">
+                <div class="confiar-info"><span class="fs24 dinnB">Atención personalizada</span></div>
+            </div>
+            <div class="confiar-item">
+                <img src="src/img/revision-minuciosa-ojos.svg" alt="">
+                <div class="confiar-info"><span class="fs24 dinnB">Revisión minuciosa de tus ojos</span></div>
+            </div>
+            <div class="confiar-item">
+                <img src="src/img/precios-accesibles.svg" alt="">
+                <div class="confiar-info"><span class="fs24 dinnB">Nuestros precios son accesibles</span></div>
+            </div>
+            <div class="confiar-item">
+                <img src="src/img/diferentes-metodos-pago.svg" alt="">
+                <div class="confiar-info"><span class="fs24 dinnB">Diversos métodos de pago</span></div>
+            </div>
+        </div>
+    </div>
+    <div class="call-action-section">
+        <div class="call-titulo"><span class="fs36 dinnB">No esperes más, déjanos proteger tu visión</span></div>
+        <div class="call-subtitulo"><span class="fs24 dinnB">Nuestros especialistas se encargarán de darte el tratamiento adecuado.</span></div>
+        <div>
+            <a href="#" class="button-agenda-cita open-acuity-form buton-body" id="agenda">
+                <div class="inline middle icon-agenda"><img src="src/img/icon-cita-yellow.svg" alt="icon-libro"></div>
+                <div class="inline middle text-button dinnB fs24">Agenda tu cita</div>
+            </a>
+        </div>
+    </div>
+    <div class="convenios-sect">
+        <div class="title-convenios">
+            <span class="fs36 dinnB">Contamos con convenios con las principales aseguradoras de México</span>
+        </div>
+        <div class="convenios-sect-items">
+            <div class="convenio-item"><img src="src/img/1-logo-gnp.jpg" alt="1-logo-gnp"></div>
+            <div class="convenio-item"><img src="src/img/2-logo-mediaccess.jpg" alt="2-logo-mediaccess"></div>
+            <div class="convenio-item"><img src="src/img/3-logo-plan-seguro.jpg" alt="3-logo-plan-seguro"></div>
+            <div class="convenio-item"><img src="src/img/4.-logo-Thona-seguros.jpg" alt="4.-logo-Thona-seguros"></div>
+            <div class="convenio-item"><img src="src/img/5-logo-mapfre.jpg" alt="5-logo-mapfre"></div>
+            <div class="convenio-item"><img src="src/img/6-logo-axa.jpg" alt="6-logo-axa"></div>
+            <div class="convenio-item"><img src="src/img/7-logo-ike.jpg" alt="7-logo-ike"></div>
+            <div class="convenio-item"><img src="src/img/8-logo-sis-nova-seguro.jpg" alt="8-logo-sis-nova-seguro"></div>
+            <div class="convenio-item"><img src="src/img/9-logo-la-latino-seguro.jpg" alt="9-logo-la-latino-seguro"></div>
+            <div class="convenio-item"><img src="src/img/10-logo-inbursa-logo.jpg" alt="10-logo-inbursa-logo"></div>
+            <div class="convenio-item"><img src="src/img/11-logo-general-de-salud-logo.jpg" alt="11-logo-general-de-salud-logo"></div>
+            <div class="convenio-item"><img src="src/img/12-logo-seguros-monterrey-logo.jpg" alt="12-logo-seguros-monterrey-logo"></div>
+            <div class="convenio-item"><img src="src/img/13-logo-dentegra.jpg" alt="13-logo-dentegra"></div>
+            <div class="convenio-item"><img src="src/img/14-logo-prevem.jpg" alt="14-logo-prevem"></div>
+        </div>
+    </div>
+    <div class="footer">
+        <span>© 2017 salauno®</span> <span class="divider-footer">|</span><img src="src/img/phone-icon.png" class="icon-size" alt="icon-telefono"> <span class="dinnbold"><?php
+        if( $_COOKIE ["typeAccess"] == "googleSeo" ){
+            echo '<a href="tel:+525541637477" class="color-yellow dinnbold" id="tel-google-seo">55 4163 7477</a>';
+        }else if( $_COOKIE ["typeAccess"] == "facebook"  ){
+            echo '<a href="tel:+525541637555" class="color-yellow dinnbold" id="tel-facebook">55 4163 7555</a>';
+        }else if( $_COOKIE ["typeAccess"] == "googleSem"  ){
+            echo '<a href="tel:+525541637463" class="color-yellow dinnbold" id="tel-google-sem">55 4163 7463</a>';
+        }else{
+            echo '<a href="tel:+525541636969" class="color-yellow dinnbold" id="tel-direct-link">55 4163 6969</a>';
+        }?>
+        </span> (Teléfono fijo)
+    </div>
+</body>
+
+</html>
